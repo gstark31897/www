a3b2be65f8984376e4c8a7ec4fe8eb8dfd166576
Gavin Stark <gstark31897@gmail.com>
2019-10-30 22:49:04 -0500

    adding website

---
+++ html/components/header.js
@@ -0,0 +1,11 @@
+class Header extends React.Component {
+    render() {
+        return <div class="header">
+               <div class="home-menu pure-menu pure-menu-horizontal pure-menu-fixed">
+                <a class="pure-menu-heading" href="">gstark.net</a>
+            </div>
+        </div>
+    }
+}
+
+window.Header = Header
